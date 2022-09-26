fbbee85a0acb45345237f2e27a578f0965bad9db
acarril <acarril@princeton.edu>
2022-09-26 11:44:46 -0400

    increase default timeout to 10s

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Scrape MINVU D.S.01 beneficiaries')
     parser.add_argument('-y','--year', type=int, help='year to scrape', required=True)
     parser.add_argument('-f', '--filename', help='filename of CSV output', required=True)
-    parser.add_argument('-t', '--timeout', default=3, type=int, help='request timeout', required=False)
+    parser.add_argument('-t', '--timeout', default=10, type=int, help='request timeout', required=False)
     args = parser.parse_args()
     df = scrape_year(args.year, timeout=args.timeout)
     df.to_csv(args.filename)
